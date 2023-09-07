9692b98034a84432c0d245a066404b61f29f160a
ayush kale <74785780+ayushkale1909@users.noreply.github.com>
2023-09-07 13:15:29 -0400

    Update Receiver.py

--- Receiver.py
+++ Receiver.py
@@ -1,18 +1,14 @@
 import socket
 import struct
 
-MCAST_GROUP = '224.1.1.1'  # Choose an appropriate multicast IP
-MCAST_PORT = 5007          # Choose an appropriate port
+MCAST_GROUP = '224.1.1.1'  
+MCAST_PORT = 5007          
 
 def multicast_receiver():
-    # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
-    # Bind to the server address
     sock.bind(('', MCAST_PORT))
 
-    # Add the socket to the multicast group
     group = socket.inet_aton(MCAST_GROUP)
     mreq = struct.pack('4sL', group, socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
